582981e67254f94253b7b22719e23a55251e3123
kingdomb <kbmajor80@gmail.com>
2025-04-02 16:38:08 -0400

    Initial commit: Vite React setup with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-100'>
+      <h1 className='text-3xl font-bold text-blue-600'>
+        Hello World from the Frontend!
+      </h1>
+    </div>
+  );
+}
+
+export default App
